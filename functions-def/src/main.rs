0698fc26ec872e509ec90c4f5698a1774e2634d3
axelc <axel.hpm.coudray@gmail.com>
2021-07-15 16:59:22 +0200

    04 - end of functions

--- functions-def/src/main.rs
+++ functions-def/src/main.rs
@@ -1,8 +1,109 @@
 
+// traits -
+trait Animal {
+    fn create(name: &'static str) -> Self;
+
+    fn name(&self) -> &'static str;
+
+    fn talk(&self) {
+        println!("{} cannot talk...", self.name());     // default function
+    }
+}
+
+struct Human {
+    name: &'static str
+}
+
+impl Animal for Human {
+    fn create(name: &'static str) -> Human {
+        Human{ name: name }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn talk(&self) {
+        println!("Hello my name is {}!", self.name());
+    }
+}
+
+struct Cat {
+    name: &'static str
+}
+
+impl Animal for Cat {
+    fn create(name: &'static str) -> Cat {
+        Cat{ name: name }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn talk(&self) {
+        println!("meeeow??!");
+    }
+}
+
+trait Summable<T> {
+    fn sum(&self) -> T;
+}
+
+impl Summable<i32> for Vec<i32> {
+    fn sum(&self) -> i32 {
+        let mut result:i32 = 0;
+        for x in self { result += *x; }
+        return result;
+    }
+}
+
+
+fn traits() {
+    println!("*** TRAITS ***");
+
+    // let h = Human{ name: "John" };
+    let h = Human::create("John 2.0");
+    h.talk();
+    
+    // let c = Cat{ name: "Yeti" };
+    let c = Cat::create("Yeti 2.0");
+    c.talk();
+
+    let animal:Human = Animal::create("Cyberpunk John 999.0 from 2077");
+    animal.talk();
+
+    // implementation of sum to other types (here Vec<i32>)
+    let a = vec![1, 2, 3];
+    println!("sum = {}", a.sum());
+}
+
 // high order functions -
+fn is_even(x: i32) -> bool {
+    x % 2 == 0
+}
+
 fn high_order_functions() {
     println!("*** HIGH ORDER FUNCTIONS ***");
 
+    // base function
+    let limit = 500;
+    let mut sum = 0;
+
+    for i in 0.. {
+        let isq = i * i;
+        if isq > limit { break; }
+        else if is_even(isq) { sum += isq; }
+    }
+    println!("basic function - loop sum = {}", sum);
+
+    // higher order function equivalent
+    let sum2 = (0..)
+        .map(|x| x*x)                   // 0, 1, 4, 9, 16, ...
+        .take_while(|&x| x < limit)     // until x < 500
+        .filter(|x| is_even(*x))        // we filter the list on even numbers -> 0, 4, 16, 36, ...
+        .fold(0, |sum, x| sum + x);     // adds each value to the sum that takes 0 as value at first and then the sum
+    println!("higher order function - loop sum = {}", sum2);
 }
 
 // closures -
@@ -106,7 +207,9 @@ fn functions_and_args() {
 fn main() {
     println!("--- FUNCTIONS ---");
 
-    // functions_and_args();
-    // methods();
+    functions_and_args();
+    methods();
     closures();
+    high_order_functions();
+    traits();
 }
